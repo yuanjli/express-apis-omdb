cfb8581b1d69adae9233746bca69ebfef73989a2
yuanjli <liyuanjienetwork@gmail.com>
2018-09-03 20:57:26 -0700

    for version control

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const expressEjslayout = require('express-ejs-layouts');
+const request = require('request');
+const app = express();
+
+// var express = require('express');
+// var app = express();
+
+app.set('view engine', 'ejs');
 
 // this adds some logging to each request
 app.use(require('morgan')('dev'));
+app.use(expressEjslayout);
+app.use(express.static(__dirname + '/static'));
 
-$('#btnSearch').on('click', function () {
-	var searchInput = $('#textBoxElement').val();	
-	app.get('/results', function(req, res) {
-		var url = "http://monkey=" + searchInput + "&red";
-		request(url, function(err, response, body){
+app.get('/', function(req, res) {
+	res.render('home');
+});
+					
+//$('#btnSearch').on('click', function () {
+// 	var searchInput = $('#textBoxElement').val();	
+app.get('/results', function(req, res) {
+	console.log('req.query:', req.query);
+	let movie = req.query.movie;
+//	var url = "http://monkey=" + searchInput + "&red";
+	request('http://www.omdbapi.com?apikey=a4444cbe&s=${movie}', function(err, response, body){
 		if(!err && response.statusCode === 200){
 			var parsedJson = JSON.parse(body);
-			res.render('home', {movies: parsedJson.Search});
+			console.log('OMDB body results:', parsedJson);
+			res.render('results', {results: parsedJson.Search});
 		}
 		else {
 			console.log(err);
@@ -22,10 +38,6 @@ $('#btnSearch').on('click', function () {
     // var url = "http://monkey=" + searchInput + "&red";
     // window.open(url);
 });
-
-
-
-
 
 var server = app.listen(process.env.PORT || 3000);
 
