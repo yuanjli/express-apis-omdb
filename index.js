d1bf665cf9ac4b19b0c9b3ffb2e1004e3c0b4417
yuanjli <liyuanjienetwork@gmail.com>
2018-09-03 21:06:19 -0700

    a working version of open movie db

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
 const express = require('express');
-const expressEjslayout = require('express-ejs-layouts');
+const expressEjsLayouts = require('express-ejs-layouts');
 const request = require('request');
 const app = express();
 
-// var express = require('express');
-// var app = express();
-
 app.set('view engine', 'ejs');
-
 // this adds some logging to each request
 app.use(require('morgan')('dev'));
-app.use(expressEjslayout);
+app.use(expressEjsLayouts);
 app.use(express.static(__dirname + '/static'));
 
 app.get('/', function(req, res) {
-	res.render('home');
+  res.render('home');
 });
-					
-//$('#btnSearch').on('click', function () {
-// 	var searchInput = $('#textBoxElement').val();	
+
 app.get('/results', function(req, res) {
-	console.log('req.query:', req.query);
-	let movie = req.query.movie;
-//	var url = "http://monkey=" + searchInput + "&red";
-	request('http://www.omdbapi.com?apikey=a4444cbe&s=${movie}', function(err, response, body){
-		if(!err && response.statusCode === 200){
-			var parsedJson = JSON.parse(body);
-			console.log('OMDB body results:', parsedJson);
-			res.render('results', {results: parsedJson.Search});
-		}
-		else {
-			console.log(err);
-			res.send('error');
-		}
-		res.send('Hello Backend!');
-	});
-    // var url = "http://monkey=" + searchInput + "&red";
-    // window.open(url);
-});
+  let movie = req.query.movie;
+  request('http://www.omdbapi.com/?apikey=a8822bb&s=' + movie, function(err, response, body) {
+    if(!err && response.statusCode === 200){
+      let parsedJson = JSON.parse(body);
+      res.render('results', { results: parsedJson.Search });
+    }
+    else {
+      res.send(err);
+    }
+  });
+})
+
+app.get('/movies/:id', function(req, res) {
+  let imdbID = req.params.id;
+  request(`http://www.omdbapi.com/?apikey=a8822bb&i=${imdbID}`, function(err, response, body) {  
+    if(!err && response.statusCode === 200){
+      let parsedJson = JSON.parse(body);
+      res.render('movie_details', { movie: parsedJson });
+    }
+    else {
+      res.send(err);
+    }
+  });
+})
 
 var server = app.listen(process.env.PORT || 3000);
 
